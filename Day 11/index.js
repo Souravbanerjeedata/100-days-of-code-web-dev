5d6999d3ba8fff6a9dbcd6280002beb8498571ca
Sourav Banerjee <sourav.banerjeejobs@gmail.com>
2024-03-30 15:47:50 +0530

    Day 11 final

--- Day 11/index.js
+++ Day 11/index.js
@@ -20,7 +20,6 @@ premiumThemes.addEventListener("click", () => {
 // selecting a theme
 
 const themes = [...themeOptions.querySelectorAll("li")];
-const currentTheme = "";
 const current = premiumThemes.querySelector(".current-theme");
 
 themes.forEach((theme) => {
@@ -32,3 +31,50 @@ themes.forEach((theme) => {
 });
 
 // open the promote section / click create
+
+const form = document.querySelector("form");
+const input = form.querySelector("input");
+const mainSection = document.querySelector(".main");
+const promoteSection = document.querySelector(".promote");
+const selectedTheme = document.querySelector(".selected-theme");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  if (input.value !== "") {
+    mainSection.style.display = "none";
+    promoteSection.style.display = "block";
+    selectedTheme.textContent = current.textContent;
+  }
+});
+
+// opening the protocol list
+
+const protocol = document.querySelector(".protocol");
+const protocolChevronDown = protocol.querySelector(".icon-tabler-chevron-down");
+const protocolList = document.querySelector(".protocol-list");
+
+protocol.addEventListener("click", () => {
+  protocolChevronDown.style.transition = ".5s";
+
+  if (protocolList.style.display === "block") {
+    protocolChevronDown.style.transform = "scaleY(1)";
+    protocolList.style.display = "none";
+  } else {
+    protocolChevronDown.style.transform = "scaleY(-1)";
+    protocolList.style.display = "block";
+  }
+});
+
+// selecting a protocol
+
+const protocols = [...protocolList.querySelectorAll("li")];
+const currentProtocol = protocol.querySelector(".selected-protocol");
+
+protocols.forEach((proto) => {
+  proto.addEventListener("click", () => {
+    protocolChevronDown.style.transform = "scaleY(1)";
+    protocolList.style.display = "none";
+    currentProtocol.textContent = proto.textContent;
+  });
+});
